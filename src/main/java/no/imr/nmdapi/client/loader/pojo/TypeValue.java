ffdcd86e9879eebfb5594d890baa035a23ba8be2
TerryHannant <thannant_github@metafu.net>
2015-09-18 10:18:21 +0200

    Change to cruise

---
+++ src/main/java/no/imr/nmdapi/client/loader/pojo/TypeValue.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package no.imr.nmdapi.client.loader.pojo;
+
+/**
+ *
+ * @author Terry Hannant <a5119>
+ */
+public class TypeValue {
+    
+    private String value;
+    private String type;
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+    
+    
+}
